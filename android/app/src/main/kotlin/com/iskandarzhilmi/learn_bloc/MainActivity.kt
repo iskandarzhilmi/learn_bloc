1e062b32c6ba39447c88653292d8a4c93bbef5c5
Iskandar <iskandarzhilmi@gmail.com>
2022-03-30 17:04:46 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/iskandarzhilmi/learn_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.iskandarzhilmi.learn_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,34 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'counter_state.dart';
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void increment() {
+    addError(Exception('increment error!'), StackTrace.current);
+    emit(state + 1);
+  }
+
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    print(change);
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    print('$error, $stackTrace');
+    super.onError(error, stackTrace);
+  }
+}
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    // TODO: implement onChange
+    super.onChange(bloc, change);
+    print('${bloc.runtimeType} $change');
+  }
+}
