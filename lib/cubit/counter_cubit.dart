22d49876548c3afa0814799778ae45282bcbe3c0
Iskandar <iskandarzhilmi@gmail.com>
2022-03-30 17:22:53 +0800

    Added counter cubit file.

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -31,4 +31,10 @@ class SimpleBlocObserver extends BlocObserver {
     super.onChange(bloc, change);
     print('${bloc.runtimeType} $change');
   }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    print('${bloc.runtimeType} $error $stackTrace');
+    super.onError(bloc, error, stackTrace);
+  }
 }
